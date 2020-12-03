5ba8d2dd2d7b868f1a4b9da0bd06434516bd0e56
jrisebor <jrisebor@gmail.com>
2020-12-03 12:24:41 -0500

    Day 3 part 2

--- 2020/3/day3.py
+++ 2020/3/day3.py
@@ -3,17 +3,20 @@ Advent of Code 2020 - day 3: https://adventofcode.com/2020/day/3
 Toboggan Trajectory
 """
 
-def ride(area_map: list, right: int) -> None:
+def ride(area_map: list, right: int, down: int) -> int:
+    """ traverse area map with given slope """
     collisions = 0
     index = 0
     width = len(area_map[0])
-    for line in area_map:
+    for line in area_map[::down]:
         wrapped_index = index % width
         if line[wrapped_index] == '#':
             collisions += 1
         index += right
 
-    print(f"Collisions: {collisions}")
+    print(f"Collisions for pattern right {right}, down {down}: {collisions}")
+    return collisions
+
 
 def map_trajectory(input_file: str) -> None:
     """ Determine the path of the toboggan """
@@ -22,8 +25,14 @@ def map_trajectory(input_file: str) -> None:
         for line in reader.readlines():
             area_map.append(list(line.strip()))
 
-    ride(area_map, 3)
-46
+    product = ride(area_map, 1, 1)
+    product *= ride(area_map, 3, 1) # part 1
+    product *= ride(area_map, 5, 1)
+    product *= ride(area_map, 7, 1)
+    product *= ride(area_map, 1, 2)
+
+    print(f"Product: {product}")
+
 
 if __name__ == "__main__":
     map_trajectory("input.txt")
